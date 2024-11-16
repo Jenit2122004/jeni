40ed2167d5dc7ba0d4e0dccf86c28b132d3c547a
a <a>
2024-11-16 11:30:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require("express");
+const app=express();
+const port=process.env.PORT || 4000;
+const cors=re1uire("cors");
+
+app.use(cors());
+app.get('/',(req,res)=>{
+    res.send("hello World!")
+})
+app.listen(3000,()=>{
+    console.log(`Example app listening at port ${port}`)  
+})
---
+++ test.js
@@ -0,0 +1,4 @@
+const fs=require("fs");
+fs.writeFile("./a.txt",name,(err,name)=>{
+    console.log(name)
+})
