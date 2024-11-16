cdd88cdcbafedd255310debb88f6ca9d42a6991c
a <a>
2024-11-16 11:32:00 +0530

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const app=express();
 const port=process.env.PORT || 4000;
-const cors=re1uire("cors");
+const cors=require("cors");
 
 app.use(cors());
 app.get('/',(req,res)=>{
